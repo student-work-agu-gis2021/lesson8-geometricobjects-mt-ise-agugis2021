6ad5f14e01edb9596034e0c0e8b218c34610a54b
mtiseagugis2021 <>
2021-06-06 15:27:52 +0000

    I added code to test function usage.

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -134,7 +134,7 @@ def get_centroid(geom):
 # 
 
 #  YOUR CODE HERE 7 to define some objects
-
+poly1 = create_poly_geom([(45.2,22.34),(100.22,-3.20),(70.0,10.20)])
 
 # CODE FOR TESTING YOUR SOLUTION
 centroid = get_centroid(poly1)
