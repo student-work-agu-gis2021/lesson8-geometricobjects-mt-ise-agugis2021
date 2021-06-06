948f30bf191fa6cd467f53e1faa88f8df51cf8e2
mtiseagugis2021 <>
2021-06-06 16:29:41 +0000

    I fixed the error.

--- Exercise_1_problem_3_4.py
+++ Exercise_1_problem_3_4.py
@@ -120,7 +120,7 @@ print('lines length:', len(lines))
 #raise NotImplementedError()
 from shapely.geometry import LineString
 for orig,dest in zip(orig_points,dest_points):
-  line=LineString([orig_points])
+  line=LineString([orig,dest])
   lines.append(line)
 # CODE FOR TESTING YOUR SOLUTION
 
